dde45bdb34eb9eecddfe01995b03d87aba79108f
petereon <pvyboch1@gmail.com>
2022-12-17 11:07:47 +0100

    feat: basic board construction, feels very imperative

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import           HaskellSay (haskellSay)
+
+data Move = Move Integer Integer
+
+data Row = Row Integer Integer Integer
+data Board = Board Row Row Row
+
+createEmptyBoard :: Board
+createEmptyBoard = Board (Row 0 0 0) (Row 0 0 0) (Row 0 0 0)
+
+makeMove :: Move Board -> Board
+
 
 main :: IO ()
 main = haskellSay
