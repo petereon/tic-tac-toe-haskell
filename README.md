29d36ab4a6c738dff7670302d108d9820bb67c42
petereon <pvyboch1@gmail.com>
2022-12-17 23:38:15 +0100

    feat: adding doctests more agressive formatter and some functionality

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 - [x] Find formatters for `.cabal` and `.hs` files
 - [x] Add some (prefferably Haskell based) file watcher to execute the formatters
 - [x] Read-up on Haskell linting
-- [ ] Read-up on testing in Haskell
+- [x] Read-up on testing in Haskell
 - [ ] Find how to control development dependencies in .cabal 
+
+Notes:
+- `stylish-haskell` feels like it's not doing much - not too suitable for learners without a faintest idea on how to style Haskell
--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,53 @@
 module Main where
 
+newtype Player = Player Integer deriving (Show)
 
-data Move = Move Integer Integer
+data Move = Move Integer Integer deriving (Show)
 
-data Row = Row Integer Integer Integer
-data Board = Board Row Row Row
+type Board = [Maybe Integer]
 
+-- | Create an empty board
+--
+-- Examples:
+-- >>> createEmptyBoard
+-- [Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing]
 createEmptyBoard :: Board
-createEmptyBoard = Board (Row 0 0 0) (Row 0 0 0) (Row 0 0 0)
+createEmptyBoard = replicate 9 Nothing
 
-makeMove :: Move Board -> Board
+-- | Create a Player
+--
+-- Examples:
+-- >>> createPlayer 1
+-- Player 1
+--
+-- >>> createPlayer 2
+-- Player 2
+createPlayer :: Integer -> Player
+createPlayer = Player
 
+-- | Translate coords from points to index
+--
+-- Examples:
+-- >>> translateCoords (Move 1 1)
+-- Just 0
+--
+-- >>> translateCoords (Move 2 2)
+-- Just 4
+--
+-- >>> translateCoords (Move 3 1)
+-- Just 6
+translateCoords :: Move -> Maybe Integer
+translateCoords (Move row column) =
+  if row > 3 || column > 3
+    then Nothing
+    else Just ((row - 1) * 3 + (column - 1))
+
+makeMove :: Move -> Board -> Player -> Board
+makeMove _ board _ = board
+
+-- createRandomMove :: Board -> Move
 
 main :: IO ()
-main = haskellSay
-    "Hello, Haskell!"
+main = do
+  let emptyBoard = createEmptyBoard
+  print emptyBoard
